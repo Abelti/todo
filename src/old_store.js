4c48262ebf70720f767d07cfa0eca269a3fd729c
Abelti <abeltibbeshana@gmail.com>
2024-08-06 11:59:27 +0300

    old_git_store

---
+++ src/old_store.js
@@ -0,0 +1,39 @@
+// src/store.jsmport { createStore, combineReducers } from 'redux';
+
+const initialState = {
+    tasks: [],
+    completedTasks: [],
+};
+
+const tasksReducer = (state = initialState.tasks, action) => {
+    switch (action.type) {
+        case 'ADD_TASK':
+            return [...state, action.payload];
+        case 'DELETE_TASK':
+            return state.filter(task => task.id !== action.payload);
+        case 'UPDATE_TASK':
+            return state.map(task => (task.id === action.payload.id ? { ...task, ...action.payload.updates } : task));
+        case 'MARK_AS_COMPLETED':
+            return state.filter(task => task.id !== action.payload);
+        default:
+            return state;
+    }
+};
+
+const completedTasksReducer = (state = initialState.completedTasks, action) => {
+    switch (action.type) {
+        case 'MARK_AS_COMPLETED':
+            return [...state, action.payload];
+        default:
+            return state;
+    }
+};
+
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    completedTasks: completedTasksReducer,
+});
+
+const store = createStore(rootReducer);
+
+export default store;
